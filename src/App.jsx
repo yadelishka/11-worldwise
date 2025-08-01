0e1d80d17bc3fbf8a673f7de1fbb16227aa34272
Adelina <yadelishka@gmail.com>
2025-08-01 17:20:32 +0400

    add Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -44,8 +44,10 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isLoading={isLoading} />}
+            element={<Navigate replace to="cities" />}
+            isLoading={isLoading}
           />
+          } />
           <Route
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
