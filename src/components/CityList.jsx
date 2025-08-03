e8a32de6b0b2f9faf623e179f626fceca48d9edb
Adelina <yadelishka@gmail.com>
2025-08-03 20:04:20 +0400

    add CitiesContext

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -2,8 +2,11 @@ import Spinner from "./Spinner";
 import Message from "./Message";
 import styles from "./CityList.module.css";
 import CityItem from "./CityItem";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,11 @@ import Spinner from "./Spinner";
 import Message from "./Message";
 import styles from "./CountryList.module.css";
 import CountryItem from "./CountryItem";
+import { useCities } from "../contexts/CitiesContext";
 
-function CityList({ cities, isLoading }) {
+function CityList() {
+  const { cities, isLoading } = useCities();
+
   if (isLoading) return <Spinner />;
 
   if (!cities.length)
