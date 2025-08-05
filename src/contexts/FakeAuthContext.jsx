3d6d31f5a8e3b416afdaccb0e27fd3d1375c1c94
Adelina <yadelishka@gmail.com>
2025-08-05 16:41:16 +0400

    add fake auth

--- src/contexts/FakeAuthContext.jsx
+++ src/contexts/FakeAuthContext.jsx
@@ -51,6 +51,7 @@ function useAuth() {
   const context = useContext(AuthContext);
   if (!context === undefined)
     throw new Error("AuthContext was used outside the provider");
+  return context;
 }
 
 export { AuthProvider, useAuth };
